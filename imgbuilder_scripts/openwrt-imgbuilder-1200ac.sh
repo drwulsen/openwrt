5be541fc9e4ef2660ea1d045d3379f7a30036072
Walter Hüttenmeyer <drwulsen@mailbox.org>
2022-11-22 22:09:55 +0100

    Working version
    Changed back from scp to rsync (preserve folder structure of copied configs for flashing)
    Passed shellcheck tests.

--- imgbuilder_scripts/openwrt-imgbuilder-1200ac.sh
+++ imgbuilder_scripts/openwrt-imgbuilder-1200ac.sh
@@ -4,14 +4,22 @@ str_dnsname="m1200.lan"
 str_port="2222"
 str_imagebuilder_path="./imagebuilder"
 str_buildprofile="linksys_wrt1200ac"
-str_logfile="./logfile"
+str_output_dir="/tmp/${str_buildprofile}"
+
+arr_rsync_opts=(
+--archive
+--verbose
+--numeric-ids
+--relative
+"--rsh=ssh -p ${str_port}"
+)
 
 #what to scp from the target and include in the image as default configuration
 arr_configs=(
 /etc/config/{dhcp,dropbear,firewall,fstab,luci,network,system,uhttpd,wireless}
 /etc/dropbear/{authorized_keys,dropbear_ed25519_host_key,dropbear_rsa_host_key}
 /etc/ssh/sshd_config
-/etc/{firewall.user,passwd,group,rc.local,shadow,sysupgrade.conf,uhttpd.crt,uhttpd.key}
+/etc/{firewall.user,group,passwd,rc.local,shadow,sysupgrade.conf,uhttpd.crt,uhttpd.key}
 /usr/local
 /www/luci-static/openwrt2020/cascade.css
 )
@@ -37,12 +45,14 @@ rsync
 arr_packages_exclude=(
 ppp
 ppp-mod-pppoe
+dnsmasq
 )
 
 #services to disable by default
 arr_disabled_services=(
 dnsmasq
 odhcpd
+firewall
 )
 
 #configs: expand to a string with root@dnsname prepended for ease of scp use
@@ -52,26 +62,28 @@ arr_configs=( "${arr_configs[@]/#/root@$str_dnsname:}" )
 arr_packages=( "${arr_packages_include[@]}" )
 arr_packages+=( "${arr_packages_exclude[@]/#/-}" )
 
-echo "Files to pull:"
-for i in ${arr_configs[@]}; do
-echo "$i";
-done
-
 mkdir -p "${str_imagebuilder_path}/files"
 rm -rf "${str_imagebuilder_path}/files/"*
-scp -rCpOP "$str_port" "${arr_configs[@]}" "${str_imagebuilder_path}/files"
+#scp -rCOP "$str_port" "${arr_configs[@]}" "${str_imagebuilder_path}/files"
+rsync "${arr_rsync_opts[@]}" --verbose "${arr_configs[@]}" "${str_imagebuilder_path}/files"
 
-#sleep 5
+#create output directory
+mkdir -p "$str_output_dir"
 
-cd "$str_imagebuilder_path"
-#make image \
+#TODO: log and exit
+cd "$str_imagebuilder_path" || exit
+
+make clean
+make image \
 PROFILE="$str_buildprofile" \
-BIN_DIR="/tmp/$str_buildprofile" \
+BIN_DIR="$str_output_dir" \
 PACKAGES="${arr_packages[*]}" \
 FILES="files" \
 DISABLED_SERVICES="${arr_disabled_services[*]}"
-cd -
+cd ..
 
 #find sysupgrade file and copy to the target
-str_sysupgrade_file=$(find -name *${buildprofile}*sysupgrade*.bin)
-scp -CpOP "$str_port" "$str_sysupgrade_file" "root@${str_dnsname}:/tmp/"
+str_sysupgrade_file=$(find "$str_output_dir" -name "*${str_buildprofile}*sysupgrade*.bin")
+echo "$str_sysupgrade_file"
+scp -COP "$str_port" "$str_sysupgrade_file" "root@${str_dnsname}:/tmp/"
+ssh -p "$str_port" root@"$str_dnsname"
